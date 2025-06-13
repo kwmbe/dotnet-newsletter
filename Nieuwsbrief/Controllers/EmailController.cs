0e7484ba65c308fe799b365201c9f562761e3ec5
kwmbe <koberkein@gmail.com>
2025-06-13 23:59:02 +0200

    Remove template

---
+++ Nieuwsbrief/Controllers/EmailController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Nieuwsbrief.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EmailController(EmailContext context) : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Post(String emailString)
+        {
+            // validate first
+
+            var email = new Email { EmailAddress = emailString };
+
+            context.Add(email);
+            await context.SaveChangesAsync();
+
+            return Created();
+        }
+    }
+}
---
+++ Nieuwsbrief/EmailContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Nieuwsbrief
+{
+    public class EmailContext : DbContext
+    {
+        public DbSet<Email> Emails { get; set; }
+
+        public string DbPath { get; }
+
+        public EmailContext()
+        {
+            var folder = Environment.SpecialFolder.LocalApplicationData;
+            var path = Environment.GetFolderPath(folder);
+            DbPath = System.IO.Path.Join(path, "email.db");
+        }
+
+        // The following configures EF to create a Sqlite database file in the
+        // special "local" folder for your platform.
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+            => options.UseSqlite($"Data Source={DbPath}");
+    }
+
+    public class Email
+    {
+        public int EmailId { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
